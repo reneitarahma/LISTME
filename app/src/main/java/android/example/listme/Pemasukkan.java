010e5918b95c394e0c4dd3e6d3b7d24db7d08760
naufaldiyusuf <naufaldi.yusuf@gmail.com>
2019-02-27 05:58:02 +0600

    first commit

---
+++ app/src/main/java/android/example/listme/Pemasukkan.java
@@ -0,0 +1,13 @@
+package android.example.listme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pemasukkan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pemasukkan);
+    }
+}
